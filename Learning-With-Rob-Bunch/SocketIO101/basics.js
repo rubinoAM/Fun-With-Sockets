7bec62496ee3e3f2e6e6bacccc184c42e2dce34e
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-04 12:53:56 -0400

    Added socket.io

--- Learning-With-Rob-Bunch/SocketIO101/basics.js
+++ Learning-With-Rob-Bunch/SocketIO101/basics.js
@@ -1,5 +1,10 @@
-const http = require('http');
+const http = require('http'); //You can also use this with Express
+const socketIo = require('socket.io');
 
 const server = http.createServer((req,res)=>{
     res.end("BARK BARK");
 });
+
+const io = socketIo(server);
+
+server.listen(7772);
