15301908ee824105b906fadcce8eb3f4f86fbac3
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-03 22:21:18 -0400

    ws.on()

--- Learning-With-Rob-Bunch/NativeWS/justWsServer.js
+++ Learning-With-Rob-Bunch/NativeWS/justWsServer.js
@@ -8,5 +8,12 @@ const server = http.createServer((req,res)=>{
 const wss = new webSocket.Server({
     server //Before ES6 this would be server: server
 });
+wss.on('headers',(headers,req)=>{
+    console.log(headers);
+})
+
+wss.on('connection',(ws,req)=>{
+    ws.send('Welcome to the jungle.')
+})
 
 server.listen(9101);
