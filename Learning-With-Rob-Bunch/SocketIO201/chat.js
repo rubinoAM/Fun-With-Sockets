0e5e521a84ea27abe5823381d3c0d827d8de172a
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-05 13:17:46 -0400

    Cleanup

--- Learning-With-Rob-Bunch/SocketIO201/chat.js
+++ Learning-With-Rob-Bunch/SocketIO201/chat.js
@@ -1,23 +1,23 @@
+//Modules
 const express = require('express');
 const socketIo = require('socket.io');
 
+//Server
 const app = express();
 app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(7773);
 const io = socketIo(expressServer);
 
-io.on('connection',(socket)=>{
+//Main Connection
+io.on('connection',(socket)=>{ //Root Namespace
     socket.emit('dataFromServer',{data:'Welcome!'});
-
     socket.on('dataToServer',(toData)=>{
-        //console.log(toData);
+        console.log(toData);
     })
-
-    socket.on('newMsgToServer',(msg)=>{
-        io.emit('msgToClients',{text:msg.text});
-    });
 });
 
+//Socket for /admin
 io.of('/admin').on('connection',(socket)=>{
     console.log("!!!!");
+    io.of('/admin').emit('welcome',"Welcome to the admin channel!");
 });
--- Learning-With-Rob-Bunch/SocketIO201/public/scripts/script.js
+++ Learning-With-Rob-Bunch/SocketIO201/public/scripts/script.js
@@ -1,12 +1,9 @@
 const socket = io('http://localhost:7773'); //Root endpoint- '/'
 const socket2 = io('http://localhost:7773/admin'); //Endpoint- '/admin'
 
+//Main Connection
 socket.on('connect',()=>{
     console.log(socket.id);
-})
-
-socket2.on('connect',()=>{
-    console.log(socket2.id);
 })
 
 socket.on('dataFromServer',(fromData)=>{
@@ -22,3 +19,12 @@ document.querySelector('#msg-form').addEventListener('submit',(e)=>{
 socket.on('msgToClients',(msg)=>{
     document.querySelector('#messages').innerHTML += `<li>${msg.text}</li>`;
 });
+
+//Socket for /admin
+socket2.on('connect',()=>{
+    console.log(socket2.id);
+})
+
+socket2.on('welcome',(msg)=>{
+    console.log(msg);
+})
